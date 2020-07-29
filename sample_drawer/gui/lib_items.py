675f5ef596608a1c2e512ffe106e1d0c409c37cc
Jacek Konieczny <jajcus@jajcus.net>
2020-07-29 20:37:33 +0200

    allow drag & drop from library items
    to external apps

--- sample_drawer/gui/lib_items.py
+++ sample_drawer/gui/lib_items.py
@@ -2,7 +2,9 @@
 import logging
 import os
 
-from PySide2.QtCore import Slot, Signal, QTimer, QObject, QItemSelection, Qt
+from urllib.parse import urlunsplit
+
+from PySide2.QtCore import Slot, Signal, QTimer, QObject, QItemSelection, Qt, QMimeData, QByteArray
 from PySide2.QtWidgets import QAbstractItemView, QCompleter
 from PySide2.QtGui import QStandardItemModel, QIcon, QStandardItem
 
@@ -12,6 +14,69 @@ logger = logging.getLogger("lib_items")
 
 ITEM_LIMIT = 100
 COMPLETION_LIMIT = 10
+
+MIMETYPES = {
+        "WAV": "audio/x-wav",
+        "OGG": "audio/ogg",
+        "FLAC": "audio/x-flac",
+        "AIFF": "audio/x-aiff",
+        }
+
+class ItemMimeData(QMimeData):
+    def __init__(self, app, items):
+        QMimeData.__init__(self)
+        self._app = app
+        self._items = [item.data() for item in items]
+        self._formats = None
+    def formats(self):
+        if self._formats is not None:
+            return self._formats
+        result = []
+        if len(self._items) == 1:
+            metadata = self._items[0]
+            mime_type = MIMETYPES.get(metadata.format, "application/octet-stream")
+            result.append(mime_type)
+        result.append("text/uri-list")
+        result.append("text/plain")
+        self._formats = result
+        return result
+    def hasFormat(self, fmt):
+        return fmt in self.formats()
+    def retrieveData(self, mime_type, var_type):
+        logger.debug("retrieveData(%r, %r)", mime_type, var_type)
+        if mime_type not in self.formats():
+            logger.debug("unsupported type requested")
+            return None
+        paths = []
+        for item in self._items:
+            path = self._app.library.get_library_object_path(item)
+            paths.append(path)
+
+        if mime_type == "text/plain":
+            return " ".join(paths)
+        elif mime_type == "text/uri-list":
+            uris = []
+            for path in paths:
+                uri = urlunsplit(("file", "localhost", path, None, None))
+                uris.append(uri.encode("utf-8"))
+            # text/uri-list is defined to use CR LF line endings
+            return QByteArray.fromRawData(b"\r\n".join(uris) + b"\r\n")
+        else:
+            path = paths[0]
+            with open(path, "rb") as data_f:
+                data = data_f.read()
+            return QByteArray.fromRawData(data)
+
+class ItemModel(QStandardItemModel):
+    def __init__(self, app):
+        QStandardItemModel.__init__(self)
+        self._app = app
+    def mimeData(self, indexes):
+        logger.debug("mimeData(%r)", indexes)
+        items = [ self.itemFromIndex(index) for index in indexes ]
+        return ItemMimeData(self._app, items)
+    def supportedDragActions(self):
+        return Qt.CopyAction
 
 class LibraryItems(QObject):
     item_selected = Signal(object)
@@ -25,7 +90,7 @@ class LibraryItems(QObject):
         self.tree_conditions = []
         self.items = []
         self.items_incomplete = False
-        self.model = QStandardItemModel()
+        self.model = ItemModel(self.app)
         self.model.setColumnCount(1)
         self.compl_model = QStandardItemModel()
         self.compl_model.setColumnCount(1)
@@ -37,6 +102,9 @@ class LibraryItems(QObject):
         self.view.setIndentation(0)
         self.view.setModel(self.model)
         self.view.sortByColumn(0, Qt.AscendingOrder)
+        #self.view.setDragDropMode(QAbstractItemView.DragOnly | QAbstractItemView.InternalMove)
+        self.view.setDragDropMode(QAbstractItemView.InternalMove)
+        self.view.setDragEnabled(True)
         self.view.setSelectionMode(QAbstractItemView.ExtendedSelection)
         selection_model = self.view.selectionModel()
         selection_model.selectionChanged.connect(self.selection_changed)
@@ -51,10 +119,14 @@ class LibraryItems(QObject):
         icon = QIcon.fromTheme("audio-x-generic")
         for item in self.items:
             s_item = QStandardItem(icon, item.name)
+            s_item.setDragEnabled(True)
+            s_item.setDropEnabled(False)
             s_item.setData(item)
             self.model.appendRow([s_item])
         if self.items_incomplete:
             s_item = QStandardItem("…and more")
+            s_item.setDragEnabled(False)
+            s_item.setDropEnabled(False)
             self.model.appendRow([s_item])
 
     @Slot(QItemSelection)
