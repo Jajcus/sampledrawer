58dbe13bf54e74618b83ea2da89f559057cf5124
Jacek Konieczny <jajcus@jajcus.net>
2020-08-31 13:21:56 +0200

    test_dsp: actually test for the peak levels
    previous code would pass even if only a single frame or single channel was analyzed

--- tests/test_dsp.py
+++ tests/test_dsp.py
@@ -14,16 +14,28 @@ class TestComputePeakLevel:
         assert level == -math.inf
 
     def test_mono_full_scale(self):
-        frames = numpy.array([[1.0], [1.0], [1.0], [1.0], [1.0]])
+        frames = numpy.array([[0.0], [1.0], [0.0], [-1.0], [0.0]])
         level = compute_peak_level(frames)
         assert level == 0
 
     def test_stereo_full_scale(self):
-        frames = numpy.array([[1.0, 1.0], [1.0, 1.0], [1.0, 1.0], [1.0, 1.0], [1.0, 1.0]])
+        frames = numpy.array([[0.0, 0.0], [0.9, 1.0], [0.0, 0.0], [-0.9, -1.0], [0.0, 0.0]])
         level = compute_peak_level(frames)
         assert level == 0
 
     def test_stereo_half_scale(self):
-        frames = numpy.array([[0.5, 0.5], [0.5, 0.5], [0.5, 0.5], [0.5, 0.5], [0.5, 0.5]])
+        frames = numpy.array([[0.0, 0.0], [0.5, 0.5], [0.0, 0.0], [-0.5, -0.5], [0.0, 0.0]])
         level = compute_peak_level(frames)
+        assert level == pytest.approx(-6.02059991)
+
+    def test_stereo_half_scale_asymmetric(self):
+        frames = numpy.array([[0.0, 0.0], [0.25, 0.25], [0.5, 0.5], [0.25, 0.25], [0.0, 0.0]])
+        level = compute_peak_level(frames)
+        # is this really the value we want here in this case?
+        assert level == pytest.approx(-6.02059991)
+
+    def test_stereo_half_scale_asymmetric_negative(self):
+        frames = numpy.array([[0.0, 0.0], [0.25, 0.25], [-0.5, -0.5], [-0.25, -0.25], [0.0, 0.0]])
+        level = compute_peak_level(frames)
+        # is this really the value we want here in this case?
         assert level == pytest.approx(-6.02059991)
